2aa9ee7768f87065abacfb03e0b1e2dd21fdffab
vinhbh <hoangvinh1577@gmail.com>
2024-07-17 00:13:15 +0700

    Update cors

--- main.go
+++ main.go
@@ -5,8 +5,11 @@ import (
 	"go-api-server/initializers"
 	"go-api-server/middlewares"
 	"go-api-server/migrate"
+	"net/http"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,6 +22,16 @@ func init() {
 func main() {
 	r := gin.Default()
 
+	// Cấu hình CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"}, // Thay thế bằng các nguồn mà bạn muốn cho phép
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	r.POST("/signup", controllers.Signup)
 	r.POST("/login", controllers.Login)
 	r.GET("/validate", middlewares.RequireAuth, middlewares.RoleAuth, controllers.Validate)
@@ -27,6 +40,10 @@ func main() {
 	r.GET("/etcd-get-prefix", controllers.EtcdGetByPrefix)
 
 	r.POST("/api/v1/gtm/config-gtm", controllers.ConfigGtm)
+
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Route not found"})
+	})
 
 	port := os.Getenv("PORT")
 	r.Run(port)
